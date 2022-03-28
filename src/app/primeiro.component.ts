c0e71c3e54b810be7a2bc98bea60b8625a38730f
Camila <cml.isa.17@gmail.com>
2022-03-28 16:27:31 -0300

    Primeiro commit app Angular

---
+++ src/app/primeiro.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-primeiro',
+  template: '<p> Meu primeiro componente Angular! </p>'
+})
+
+export class PrimeiroComponent{}
